850b8df9424e7ce22b2406bd31186d55e65444b9
AiratGimaev <gimaev_1996@mail.ru>
2017-10-20 02:25:11 +0400

    Added philosopher realization through concurrent

---
+++ src/main/java/ru/epamcourses/homework/philosopher/concurrent/domain/Philosopher.java
@@ -0,0 +1,49 @@
+package ru.epamcourses.homework.philosopher.concurrent.domain;
+
+import ru.epamcourses.homework.philosopher.Fork;
+
+import java.time.LocalTime;
+
+public class Philosopher implements Runnable {
+
+    private final Fork leftFork;
+    private final Fork rightFork;
+    private final String name;
+
+    private static LocalTime localTime = LocalTime.now().plusSeconds(5);
+
+    public Philosopher(Fork leftFork, Fork rightFork, String name) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+        this.name = name;
+    }
+
+    private void eating() {
+        System.out.println("Philosopher " + name + " eating");
+    }
+
+    private void thinking() throws InterruptedException {
+        System.out.println("Philosopher " + name + " thinking");
+        Thread.sleep(100);
+    }
+
+    private int timeOfWork() {
+        return localTime.getSecond() - LocalTime.now().getSecond();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (timeOfWork() != 0) {
+                leftFork.lock();
+                rightFork.lock();
+                eating();
+                leftFork.unlock();
+                rightFork.unlock();
+                thinking();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
