1ed6df824b4a848e462d9de903136eaae92a1330
AiratGimaev <gimaev_1996@mail.ru>
2017-10-20 02:24:29 +0400

    Added exception that bank can throw

---
+++ src/main/java/ru/epamcourses/homework/bank/exception/NoMoneyException.java
@@ -0,0 +1,7 @@
+package ru.epamcourses.homework.bank.exception;
+
+public class NoMoneyException extends Exception {
+    public NoMoneyException(String message) {
+        super(message);
+    }
+}
